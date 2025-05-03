367c6307823ccdf78ffacd1e51355f5a8987c1b5
Pongporn Supa <pongporn.supa@gmail.com>
2025-05-03 16:47:34 +0100

    add hint messages, reduce questions to 5

--- handler.py
+++ handler.py
@@ -6,21 +6,21 @@ from collections import deque
 
 HOST, PORT = "0.0.0.0", 8888
 
+
 class TCPHandler(socketserver.BaseRequestHandler):
     def __init__(self, *args, **kwargs):
-        self.server_instance = kwargs.pop('server_instance')
+        self.server_instance = kwargs.pop("server_instance")
         super().__init__(*args, **kwargs)
 
     def handle(self):
-        logger.info("Client connected: {}", self.client_address)
         data = self.request.recv(1024).strip()
-        logger.info("Received: {}", data.decode('utf-8'))
+        logger.info("Received: {}", data.decode("utf-8"))
 
         if len(self.server_instance.messages_queue) != 0:
             try:
                 payload = self.server_instance.messages_queue.popleft()
-                self.request.sendall(payload.encode("utf-8"))
                 logger.info("Sent message to client: {}", payload)
+                self.request.sendall(payload.encode("utf-8"))
             except Exception as e:
                 logger.error("Error sending message to client: {}", e)
 
@@ -29,10 +29,11 @@ class Server:
     def __init__(self, mode: Literal["robot", "human"], host=HOST, port=PORT):
         self.server = socketserver.TCPServer(
             (host, port),
-            lambda *args, **kwargs: TCPHandler(*args, **kwargs, server_instance=self))
+            lambda *args, **kwargs: TCPHandler(*args, **kwargs, server_instance=self),
+        )
         self.mode = mode
         self.client_socket = None
-        self.messages_queue = deque()
+        self.messages_queue: deque[str] = deque()
         logger.info("Server started on port {}", port)
 
         self.thr = threading.Thread(target=self.server.serve_forever)
@@ -52,9 +53,11 @@ class Server:
         self.server.shutdown()
         self.server.server_close()
         logger.info("Server closed")
+
     def set_client_socket(self, client_socket):
         self.client_socket = client_socket
         logger.info("Client socket set: {}", self.client_socket)
 
     def send(self, message: str):
+        logger.info(self.messages_queue)
         self.messages_queue.append(f"{self.mode}:{message}")
---
+++ mappings.py
@@ -0,0 +1,65 @@
+ENCOURAGEMENT_MESSAGES = {
+    "CF": [
+        "Outstanding work!",
+        "You're on fire!",
+        "Brilliant thinking!",
+        "That was impressive!",
+        "You nailed it!",
+    ],
+    "CS": [
+        "You're getting the hang of it!",
+        "Keep going, you're making progress.",
+        "You're doing great, keep it up.",
+        "That was a tough one, but you got it!",
+        "Nice effort, let's keep pushing forward.",
+    ],
+    "WF": [
+        "No worries, let's try again.",
+        "You're close, think it through.",
+        "Good effort, let's take another shot at it.",
+        "Mistakes help us learn, let's keep going!",
+        "Almost there, let's give it another go.",
+    ],
+    "WS": [
+        "I see where you're going with that.",
+        "It's okay, take your time to figure it out.",
+        "That was a tricky one, but you'll get the next.",
+        "You're learning, and that's what matters.",
+        "Let's break it down step by step.",
+    ],
+    "IN": [
+        "You can do this!",
+        "Take your time, no rush",
+        "Almost there!",
+        "You got this!",
+        "Believe in yourself. You are smarter than you think!",
+        "Stay confident and don’t overthink",
+        "Stay calm and focused!",
+    ],
+}
+
+HINT_MESSAGES = {
+    1: "Pattern decreases continuously",
+    # 2: "Squares are involved",
+    2: [
+        "Only one side is required to calculate the volume of a cube",
+        "The volume of a cube is one side multiple by itself three times",
+    ],
+    # 4: "The left column remains unchanged in the third column of each row",
+    # 5: "There is no rotation involve",
+    3: [
+        "It's not 8",
+        "It's not 5",
+        "It's not 4",
+        "It's not 7",
+    ],
+    5: ["It's not Lead", "It's not Auras", "It's not Abode", "It's not Loop"],
+}
+
+SOUND_FILES = {
+    "CF": "0",
+    "CS": "1",
+    "WF": "2",
+    "WS": "3",
+    "IN": "4",
+}
